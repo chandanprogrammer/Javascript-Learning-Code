a11e07b9cf61e2a992d983effe4cb19d9db1d5cd
chandan <chandankumar070501@gmail.com>
2023-12-31 08:20:59 +0530

    JavaScript Theory Added

--- 01_Basics/00_javascript_theory.js
+++ 01_Basics/00_javascript_theory.js
@@ -1,8 +1,21 @@
+// -> JavaScript is a clint side scripting language.
+// -> It is used to make web page alive.
+// -> JavaScript can execute not only in the browser but also on the server
+// -> JavaScript was inverted by 'Brendan Eich' in 1995
+// -> JavaScript has evolved significantly as a language and is now used to perform a wide variety of tasks.
+
 // ---> How to execute JavaScript?
 // 1. JavaScript can be executed directly inside the browser console
 // 2. Another way to execute JavaScript is with a runtime like NodeJs that can be installed and used to run JavaScript code. 
 // 3. Another way to execute JavaScript is to insert it inside the <script> tag of an HTML document.
 // Using github codespaces
+
+// JS file ---> Browser ----> executes
+
+// ------> What makes JavaScript unique
+// HTML/CSS support
+// Simple things (simple API [Application Programming Interface])
+// Major modern browser support
 
 // V8 engine is a JavaScript engine
 // NodeJs makes in c++ language by 'Ryan Dahl'
@@ -11,5 +24,14 @@
 // ECMAScript is a standared on which JavaScript is passed. 
 // It was created to ensure that different documents on JavaScript are actually talking about the same language.
 // JavaScript and ECMAScript can almost always be used interchangeably
+// ECMA-262 is the official name of the standard and ECMAScript is the official name of the language.
+
+// -----> JavaScript versions
+// ES1 - 1997 [ECMAScript 1st version]
+// ES2 - 1998 [ECMAScript 2nd version]
+// ES3 - 1999 [ECMAScript 3rd version]
+// ES5 - 2009 
+// ES6 - 2015 [ECMAScript]
+// Yearly version - 2016, 2017, 2018, 2019, 2020
 
 // Syntax -> Some set of rules follow during writing a JavaScript program called syntax.  
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Javascript-Learning-Code
-In this Repo step by step leran javascript code from basic to advance.     
+In this Repo step by step Leran Javascript Code from Basic to Advance.     
 
 ### Javascript Runtime Environments
 1. Node 
@@ -12,3 +12,11 @@ Install Node or Deno ( Founder - Ryan Dahl ) in your computer for run javascript
 
 ### ECMAScript Referrances
 https://tc39.es/ecma262/
+
+# JavaScript Syllabus
+- JavaScript vs ECMAScript
+- Install vs code or use github codespaces
+- Write first basic program 
+
+### Basics of JavaScript programming
+- Variable - var, let,  const
